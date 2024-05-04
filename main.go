93ed05a4badc403b55af13a9005d2144a3add869
assilbekov <askhat.assilbekov@gmail.com>
2024-05-04 12:06:09 -0300

    use decorator pattern

--- main.go
+++ main.go
@@ -1,3 +1,26 @@
-package decorator
+package main
 
-func main() {}
+import (
+	"decorator/pkg"
+	"fmt"
+)
+
+var (
+	base = &pkg.BasePC{}
+	home = &pkg.HomePC{
+		CPU:          4,
+		GraphicsCard: 2,
+		Wrapper:      base,
+	}
+	server = &pkg.ServerPC{
+		CPU:     8,
+		Memory:  64,
+		Wrapper: base,
+	}
+)
+
+func main() {
+	fmt.Println("Base PC price: " + fmt.Sprintf("%f", base.GetPrice()))
+	fmt.Println("HomePC price: " + fmt.Sprintf("%f", home.GetPrice()))
+	fmt.Println("ServerPC price: " + fmt.Sprintf("%f", server.GetPrice()))
+}
